87c5a039f2cb4a847a873fff6fe7414732b81884
Jhonatan <jhonatan.dev90@gmail.com>
2023-12-13 14:10:33 -0300

    Criação do botão 'limpar lista', reorganização do layout dos itens da lista.

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const input = document.getElementById("input");
 // Criando uma constante para capturar o button
 const button = document.getElementById("button");
 // Criando uma constante para capturar a 'ul/lista'
-const listaTarefasCompleta = document.getElementById('list')
-
+const listaTarefasCompleta = document.getElementById("list");
+// Criando uma constante para capturar o botão limpar lista'
+const limparLista = document.getElementById("limpar-lista");
 
 // Criação de um array para armazenar as tarefas digitadas no input (vai começar vazia e sendo preenchida conforme as tarefas forem sendo criadas)
 let tarefas = [];
@@ -15,12 +16,11 @@ function adicionarTarefa() {
   tarefas.push({
     tarefa: input.value,
     concluida: false,
+    excluida: false,
+  });
 
-    excluida: false
-});
-  
   //Para limpar o input todas vez que uma nova tarefa for adicionada
-  input.value = ''   
+  input.value = "";
 
   // Chamando a função exibir tarefas
   exibirTarefas();
@@ -28,67 +28,69 @@ function adicionarTarefa() {
 
 // Para exibir as tarefas criadas, e que foram inseridas na lista, na tela para que o usuario possa ver.
 function exibirTarefas() {
-  let listaTarefas = '';
+  let listaTarefas = "";
 
   tarefas.forEach((item, index) => {
-    listaTarefas = listaTarefas + `
-        <li class="itens-list ${item.concluida && "concluida" || item.excluida && "excluida"}">
+    listaTarefas =
+      listaTarefas +
+      `
+        <li class="itens-list ${
+          (item.concluida && "concluida") || (item.excluida && "excluida")
+        }">
+            <p class="texto-tarefa">${item.tarefa}</p>
             <img
-                src="./images/icon-check.png"
-                alt="imagem de check"
-                onclick="concluirTarefa(${index})"
+              src="./images/icon-check.png"
+              alt="imagem de check"
+              onclick="concluirTarefa(${index})"
             />
-            <p>${item.tarefa}</p>
             <img src="./images/icon-trash.png" alt="imagem lixeira" onclick="deletarItem(${index})"/>
         </li>
         
         `; // O 'index' se refere a posição que o botão foi clicado.
   });
 
-    // Adicionar as tarrefas criadas ao arquivo html
-    listaTarefasCompleta.innerHTML = listaTarefas
+  // Adicionar as tarrefas criadas ao arquivo html
+  listaTarefasCompleta.innerHTML = listaTarefas;
 
-
-    // Para não resetar a lista apos atualizar o navegador
-    localStorage.setItem('lista', JSON.stringify(tarefas)) //JSON.stringify transformara o objeto em string
-
-
-
+  // Para não resetar a lista apos atualizar o navegador
+  localStorage.setItem("lista", JSON.stringify(tarefas)); //JSON.stringify transformara o objeto em string
 }
 // Para alterar a cor depois de a tarefa ser marcada como concluida
 function concluirTarefa(index) {
-    // Alterando o valor do item concluido de dentro do objeto da funcão 'adicionarTarefa()' (o novo valor será ele mesmo invertivo - !(sinal de negação))
-    tarefas[index].concluida = !tarefas[index].concluida
+  // Alterando o valor do item concluido de dentro do objeto da funcão 'adicionarTarefa()' (o novo valor será ele mesmo invertivo - !(sinal de negação))
+  tarefas[index].concluida = !tarefas[index].concluida;
 
-    // Chamando a função exibir tarefas mais uma vez (para poder deletar o item certo da lista de tarefas existentes)
-    exibirTarefas()
+  // Chamando a função exibir tarefas mais uma vez (para poder deletar o item certo da lista de tarefas existentes)
+  exibirTarefas();
 }
-
 
 // Para deletar um item da lista (ela sera chamada na segunda img)
 function deletarItem(index) {
+  // Alterando o valor do item concluido de dentro do objeto da funcão 'adicionarTarefa()' (o novo valor será ele mesmo invertivo - !(sinal de negação))
+  tarefas[index].excluida = !tarefas[index].excluida;
 
-    // Alterando o valor do item concluido de dentro do objeto da funcão 'adicionarTarefa()' (o novo valor será ele mesmo invertivo - !(sinal de negação))
-    tarefas[index].excluida = !tarefas[index].excluida
-
-    // Chamando a função exibir tarefas mais uma vez (para poder deletar o item certo da lista de tarefas existentes)
-    exibirTarefas()
+  // Chamando a função exibir tarefas mais uma vez (para poder deletar o item certo da lista de tarefas existentes)
+  exibirTarefas();
 }
 
+// Função para limpar a lista de tarefas
+function limparListaTarefas() {
+  tarefas = [];
+  exibirTarefas();
+}
+limparLista.addEventListener("click", limparListaTarefas);
 
 // Para não resetar a lista apos atualizar o navegador
 function recarregarTarefas() {
-    const tarefasLocalStorag = localStorage.getItem('lista')
+  const tarefasLocalStorag = localStorage.getItem("lista");
 
-    if(tarefasLocalStorag){
-        tarefas = JSON.parse(tarefasLocalStorag)//JSON.stringify transformara a string em um objeto.ßß
-    }
+  if (tarefasLocalStorag) {
+    tarefas = JSON.parse(tarefasLocalStorag); //JSON.stringify transformara a string em um objeto.ßß
+  }
 
-    // Chamando a função exibir tarefas mais uma vez (para poder deletar o item certo da lista de tarefas existentes)
-    exibirTarefas()
+  // Chamando a função exibir tarefas mais uma vez (para poder deletar o item certo da lista de tarefas existentes)
+  exibirTarefas();
 }
 
-recarregarTarefas()
+recarregarTarefas();
 button.addEventListener("click", adicionarTarefa);
-
-
