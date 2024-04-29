c4616605b142b239ac347958c1e2d7d9e39a1291
Jhonatan <jhonatan.dev90@gmail.com>
2024-04-29 15:55:58 -0300

    Criando os elementos HTML de forma dinânmica via JavaScript.

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-// Criando uma constante para capturar o input
-const input = document.getElementById("input");
-// Criando uma constante para capturar o button
-const button = document.getElementById("button");
+// Criando os elementos HTML de forma dinânmica via JavaScript.
+// Criando o elemento div com id "container"
+let container = document.createElement("div");
+container.id = "container";
+
+// Criando o elemento h2 com texto "To Do List"
+let h2 = document.createElement("h2");
+h2.textContent = "To Do List";
+container.appendChild(h2);
+
+// Criando o elemento input com id "input" e tipo "text"
+let input = document.createElement("input");
+input.id = "input";
+input.type = "text";
+input.placeholder = "Adicionar item a lista";
+container.appendChild(input);
+
+// Criando o elemento button com id "button" e texto "Adicionar Tarefa"
+let button = document.createElement("button");
+button.id = "button";
+button.textContent = "Adicionar Tarefa";
+button.addEventListener("click", adicionarTarefa);
+container.appendChild(button);
+
+// Criando o elemento ul com id "list"
+let ul = document.createElement("ul");
+ul.id = "list";
+container.appendChild(ul);
+
+// Criando o elemento button com id "limpar-lista", texto "Limpar Lista" e atributo onclick para chamar a função limparListaTarefas()
+let limparListaBtn = document.createElement("button");
+limparListaBtn.id = "limpar-lista";
+limparListaBtn.textContent = "Limpar Lista";
+limparListaBtn.addEventListener("click", limparListaTarefas);
+container.appendChild(limparListaBtn);
+
+// Adicionando o container diretamente ao corpo da página
+document.body.appendChild(container);
+
 // Criando uma constante para capturar a 'ul/lista'
 const listaTarefasCompleta = document.getElementById("list");
 // Criando uma constante para capturar o botão limpar lista'
@@ -43,7 +78,7 @@ function exibirTarefas() {
             } onchange="concluirTarefa(${index})"/>
             <img src="./images/icon-trash.png" alt="imagem lixeira" onclick="deletarItem(${index})"/>
         </li>
-        
+
         `; // O 'index' se refere a posição que o botão foi clicado.
   });
 
